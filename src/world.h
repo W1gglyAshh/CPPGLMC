32b7df7515b3abaf4403e5324fc85a59e10a5b78
Gabriel LU <gabriellu4815@hotmail.com>
2025-03-02 18:39:10 +0800

    updated cmakelists for windows compatibility (still have issue)

---
+++ src/world.h
@@ -0,0 +1,17 @@
+// this file is for world class - the main class of the game which manages all the other classes (except Window class)
+//
+// created by Gabriel LU on Mar 2, 2025
+//
+
+#pragma once
+
+#include "include.h"
+#include "block.h"
+#include "shader.h"
+#include "camera.h"
+
+class World
+{
+private:
+public:
+};
